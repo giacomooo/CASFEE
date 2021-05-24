d167eb5b78870058289eaab9b32bb6fedf3fed2b
giacomooo <49695558+giacomooo@users.noreply.github.com>
2021-05-24 20:56:15 +0200

    Add files via upload

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+// Controller
+
+/* eslint-disable no-console */
+const btnTheme = document.getElementById('btnTheme');
+const btnAddNote = document.getElementById('btnAddNote');
+const btnShowFinished = document.getElementById('btnShowFinished');
+const btnSort = document.getElementById('btnSort');
+
+btnTheme.addEventListener('click', () => {
+  console.log(btnTheme.options[btnTheme.selectedIndex].value);
+  document.body.classList.toggle(btnTheme.options[btnTheme.selectedIndex].value);
+  // document.body.classList.toggle('dark-theme');
+});
+
+btnAddNote.addEventListener('click', () => {
+  console.log('addClicked');
+});
+
+btnShowFinished.addEventListener('click', () => {
+  console.log('ShowFinishedClicked');
+});
+
+btnSort.addEventListener('click', () => {
+  console.log('sort', btnSort.value);
+});
