ea1d0e7f8f5243dadc97f80b93ac1f5945e63541
Giacomo <Giacomo@felsenstern.ch>
2021-05-27 23:32:28 +0200

    MVC angewendet. moment.js angewendet.

---
+++ public/scripts/services/note.js
@@ -0,0 +1,52 @@
+export class Note {
+  constructor(id, dueAt, title, importance, finishedAt, description) {
+    this.id = id;
+    this.dueAt = dueAt;
+    this.title = title;
+    this.importance = importance;
+    this.finishedAt = finishedAt;
+    this.description = description;
+    this.isFinished = Boolean(this.finishedAt);
+    this.finishedToday = this.isFinishedToday(finishedAt);
+  }
+
+  isFinishedToday() {
+    const inputDate = new Date(this.finishedAt);
+    const todaysDate = new Date();
+    return inputDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0);
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      dueAt: this.dueAt,
+      title: this.title,
+      importance: this.importance,
+      description: this.description,
+      finishedToday: this.finishedToday,
+      isFinished: this.isFinished,
+    };
+  }
+}
+
+// function compareSongs(s1, s2) {
+//   return s2.rating - s1.rating;
+// }
+//
+// function songsSorted() {
+//   return [...songs].sort(compareSongs);
+// }
+//
+// function findSong(id) {
+//   return songs.find((song) => parseInt(id) === parseInt(song.id));
+// }
+
+// function rateSong(songId, delta) {
+//   const song = findSong(songId);
+//
+//   if (song) {
+//     song.rating += delta;
+//     return true;
+//   }
+//   return false;
+// }
