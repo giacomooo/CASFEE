7c9925fbf7d1894ebd83b4332c2b78bbc15f4b34
Giacomo <Giacomo@felsenstern.ch>
2021-05-31 00:14:39 +0200

    Sort Implemented

--- public/scripts/services/note-service.js
+++ public/scripts/services/note-service.js
@@ -1,4 +1,4 @@
-import { Note } from './note.js';
+import {Note} from './note.js';
 
 export class NoteService {
   constructor() {
@@ -6,7 +6,7 @@ export class NoteService {
   }
 
   loadData() {
-    const defaultNote = new Note(2, '2020-08-19', 'Some2Titel', 5, '', '22222222222222 lasfjsalfjjl lsj lkj sfljs f');
+    const defaultNote = new Note(2, '2020-08-19', 'Some2Titel', 5, '', '22222222222222 lasfjsalfjjl lsj lkj sfljs f', '2020-03-08');
     const myNote = new Note(
         1,
         '2020-08-08',
@@ -14,11 +14,25 @@ export class NoteService {
         3,
         '2020-08-09',
         'Beschreibung',
-    )
-    this.notes.push(defaultNote.toJSON());
+        '2020-08-08',
+    );
+    this.notes.push(defaultNote);
     this.notes.push(myNote);
 
-    this.notes.push(new Note(3, '2020-01-20', 'Some33Titel', 1, '2021-05-27', '33333333333333333 lasfjsalfjjl lsj lkj sfljs f'));
+    this.notes.push(new Note(3, '2020-01-20', 'Some33Titel', 1, '2021-05-27', '33333333333333333 lasfjsalfjjl lsj lkj sfljs f', '2020-01-08'));
+  }
+
+  sort(field) {
+    this.notes.sort((a, b) => {
+      if (a[field] < b[field]) {
+        return -1;
+      }
+
+      if (a[field] > b[field]) {
+        return 1;
+      }
+      return 0;
+    });
   }
 }
 
